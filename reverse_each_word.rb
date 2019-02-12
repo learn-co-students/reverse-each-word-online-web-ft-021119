86160ae03237747446ab6fbbae9d8d58ca30acea
Mark Baruch <mbaruch28@gmail.com>
2019-02-12 21:11:54 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+pizza = ("Im so hungry, I wonder what lunch is")
+
+def lunch = (pizza)
+  lunch.each do |pizza|
+    puts "#{{pizza, I hope its pizza}"
+end
+
+pizza = ("Im so hungry, I wonder what lunch is")
+
+def lunch = (pizza)
+  lunch.collect do |pizza|
+     "#{{pizza}, I hope its pizza}"
+end
